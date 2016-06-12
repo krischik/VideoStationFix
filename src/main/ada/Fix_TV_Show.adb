4d0b48b85fd793aa02048bf35ac99cfdeafbd2d6
Martin Krischik <martin.krischik@noser.com>
2016-06-12 16:53:32 +0200

    extract season and episode from (iTunes generated) file name)

--- src/main/ada/Fix_TV_Show.adb
+++ src/main/ada/Fix_TV_Show.adb
@@ -15,18 +15,43 @@
 --  along with this program.  If not, see «http://www.gnu.org/licenses/».
 ------------------------------------------------------------ }}}1 ---------
 
+pragma Assertion_Policy (Check);
+
 with Ada.Text_IO;
 with Ada.Command_Line;
+with GNAT.Regpat;
 
 procedure Fix_TV_Show is
+   Pattern : constant GNAT.Regpat.Pattern_Matcher :=
+      GNAT.Regpat.Compile ("([[:digit:]]{1,3}) - ([[:digit:]]{1,3})");
+
+   function Get_Sub_String
+      (Source   : in String;
+       Location : in GNAT.Regpat.Match_Location) return String with
+      Pre => Location.First >= Source'First and then Source'Last >= Location.Last;
+
+   function Get_Sub_String
+      (Source   : in String;
+       Location : in GNAT.Regpat.Match_Location) return String
+   is
+   begin
+      return Source (Location.First .. Location.Last);
+   end Get_Sub_String;
+
 begin
    for I in 1 .. Ada.Command_Line.Argument_Count loop
       declare
          File_Name : constant String := Ada.Command_Line.Argument (I);
+         Matches   : GNAT.Regpat.Match_Array (0 .. GNAT.Regpat.Paren_Count (Pattern));
       begin
-         Ada.Text_IO.Put_Line (File_Name);
+         GNAT.Regpat.Match (Pattern, File_Name, Matches);
 
+         Ada.Text_IO.Put (File_Name);
+         Ada.Text_IO.Put (" Season: ");
+         Ada.Text_IO.Put (Get_Sub_String (File_Name, Matches (1)));
+         Ada.Text_IO.Put (", Episode: ");
+         Ada.Text_IO.Put (Get_Sub_String (File_Name, Matches (2)));
+         Ada.Text_IO.New_Line;
       end;
    end loop;
-   Ada.Text_IO.Put_Line ("Hello, world!");
 end Fix_TV_Show;
